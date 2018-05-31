2c4fb37b8e3e9797df03256e6d13b179c7cdbc57
Turner Thornberry <turner.thornberry@gmail.com>
2018-05-31 17:52:56 -0500

    final build. 45 wow sounds

--- WowApp/ViewController.swift
+++ WowApp/ViewController.swift
@@ -17,18 +17,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate  {
     
     @IBAction func facePress(_ sender: UIButtonX) {
         //arc4random makes random number between 0 and upper bound -1, therefore +1 needed to counteract
-        let path = Bundle.main.path(forResource: "Wow\(Int(arc4random_uniform(2))+1).mp3", ofType:nil)!
+        let path = Bundle.main.path(forResource: "Wow\(Int(arc4random_uniform(45))+1).mp3", ofType:nil)!
         let url = URL(fileURLWithPath: path)
-        
-        
-        
         do {
             wowSound = try AVAudioPlayer(contentsOf: url)
             wowSound?.delegate = self // sets av player delegate protocol
             wowSound?.play()
-            face.setImage(#imageLiteral(resourceName: "Wow.jpg"), for: .normal)
-            
-            
+            face.setImage(#imageLiteral(resourceName: "Wow_NBG.png"), for: .normal)
             UIView.animate(withDuration: (wowSound?.duration)!, animations: ({
                 //first one rotates 180 degrees over half the time of the clip duration
                 self.face.transform = CGAffineTransform(scaleX: 100.0, y: 100.0)
@@ -38,30 +33,20 @@ class ViewController: UIViewController, AVAudioPlayerDelegate  {
             UIView.animate(withDuration: (wowSound?.duration)!/2, animations: ({
                 //first one rotates 180 degrees over half the time of the clip duration
                 self.face.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-                
-                }))
-            
+            }))
             //then one with delay that is 1/2 the time of the OG clip, roatates 360
             UIView.animate(withDuration: (wowSound?.duration)!/2, delay: (wowSound?.duration)!/4, options: UIViewAnimationOptions.curveEaseIn, animations: { () -> Void in
                 self.face.transform = CGAffineTransform(rotationAngle: CGFloat.pi * 2.0)
             }, completion: nil)
-            
-            
         } catch {
-            
             print("couldn't load file :(")
         }
         print("pressed")
     }
-    
-    
-    
     //declare audioplayer delegate as self in do func, this executes after audio finsihes playing
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        face.setImage(#imageLiteral(resourceName: "Owen.jpg"), for: .normal)
+        face.setImage(#imageLiteral(resourceName: "Owen_NBG.png"), for: .normal)
         print("finished playing")
-        
     }
-    
 }
 
